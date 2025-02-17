e62242d88eebad65ebd6c596781a13cb57c4de0e
Ravinamohite07 <ravinamohite527@gmail.com>
2025-02-17 23:05:30 +0530

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+
+app.use("/",(req,res) =>{
+    res.send("hello from the server...!");
+});
+
+app.use("/test",(req,res) => {
+    res.send("hello from the test..!")
+})
+
+app.listen(3000,() =>{
+    console.log("Server is successfully listen on port 3000");
+});
